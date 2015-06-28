c27e7cc8a77f01a90fa01935c79983e191788d47
dmerson <dmerson@email.arizona.edu>
2015-06-27 20:46:27 -0700

    Jasmine spy test for example.

--- spec/first.spec.js
+++ spec/first.spec.js
@@ -4,4 +4,16 @@ describe("Expect",function(){
             expect(true).toEqual(true);
         })
     });
+    describe("awesome function",function(){
+        it("has to be called",function(){
+            var thisIs={
+                awesome:function(){
+                    console.log("AWESOME!");
+                }
+            }
+            spyOn(thisIs,"awesome");
+            var x=thisIs.awesome();
+            expect(thisIs.awesome).toHaveBeenCalled();
+        })
+    });
 });
